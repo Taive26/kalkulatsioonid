54f6c3b9263b5d5cd4501bcbda207f78eea3ac4c
Taive Sarnik <taive.sarnik@gmail.com>
2021-03-09 12:56:34 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3 title-msg">
+          {{ msg }}
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data: () => ({})
+};
+</script>
---
+++ tests/unit/example.spec.js
@@ -0,0 +1,28 @@
+// Imports
+import HelloWorld from "@/components/HelloWorld.vue";
+import Vuetify from "vuetify";
+
+// Utilities
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+
+describe("HelloWorld.vue", () => { // Nimetus - komponendi nimi
+
+  const localVue = createLocalVue();
+  let vuetify;
+
+  // Siia panna kõik, mida peaks tegeme enne iga testi jooksutamist
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  it("should take text from msg prop", () => { // Nimetus - mida test peaks tegema
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      localVue,
+      vuetify,
+      propsData: { msg }
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+
+});
