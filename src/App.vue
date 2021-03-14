ad8da296f98c3722951ca736fc01e16133f68dd1
Triin <73930703+Ititriin@users.noreply.github.com>
2021-03-14 11:52:40 +0200

    tabs enable chosen calculator content, side panes are static

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      light
-      flat
-    >
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
-
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
+    <v-app-bar app color="primary" light flat>
+      <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab
-          v-for="link in links"
+          v-for="(link, i) in links"
           :key="link"
+          @click="current = i"
+          :class="{ current: i == current }"
         >
           {{ link }}
         </v-tab>
       </v-tabs>
-
     </v-app-bar>
 
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
+          <!--vasakpoolne osa    -->
+          <v-col cols="12" sm="2" class="d-none d-sm-flex">
+            <v-sheet rounded="lg" min-height="268">
+              <div>
+                <leftpane />
+              </div>
             </v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
+          <!--keskmine osa -->
+          <v-col cols="12" sm="8">
+            <v-sheet min-height="70vh" rounded="lg">
+              <div v-show="current === 0">
+                <salary />
+              </div>
+              <div v-show="current === 1">
+                <fuel />
+              </div>
+              <div v-show="current === 2">
+                <coffee /></div
+            ></v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
+          <!--parempoolne osa -->
+
+          <v-col cols="12" sm="2" class="d-none d-sm-flex">
+            <v-sheet rounded="lg" min-height="268">
+              <div>
+                <rightpane />
+              </div>
             </v-sheet>
           </v-col>
         </v-row>
@@ -71,20 +55,25 @@
 </template>
 
 <script>
+import salary from "@/views/Salary";
+import coffee from "@/views/Coffee";
+import fuel from "@/views/Fuel";
+import leftpane from "@/views/LeftPane";
+import rightpane from "@/views/RightPane";
 
 export default {
   name: "App",
-
   components: {
-    
+    salary,
+    coffee,
+    fuel,
+    leftpane,
+    rightpane,
   },
-
- data: () => ({
-      links: [
-        'PALGAKALKULAATOR',
-        'KÜTUSEKALKULAATOR',
-        'KOFEIINIKALKULAATOR',
-      ],
-    }),
+  data: () => ({
+    links: ["PALGAKALKULAATOR", "KÜTUSEKALKULAATOR", "KOFEIINIKALKULAATOR"],
+    current: 1,
+  }),
 };
 </script>
+<style scoped></style>
