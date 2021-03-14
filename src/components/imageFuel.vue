165e430b05df636bff20c80a4a8ca7368dfe18c0
Triin <73930703+Ititriin@users.noreply.github.com>
2021-03-14 11:50:26 +0200

    add new image components

---
+++ src/components/imageFuel.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <img :src="image"/>
+    </div>
+</template>
+    
+<script>
+    import image from "@/assets/gas.png"
+    
+    export default {
+        data: function () {
+            return {
+                image: image
+            }
+        }
+    }
+</script>
+    
+<style lang="css">
+</style>
