3b7b0c88423f2e6a96b4366f688ed0ac7d8c3673
Taive Sarnik <taive.sarnik@gmail.com>
2021-03-15 18:03:17 +0200

    input design

--- src/views/Salary.vue
+++ src/views/Salary.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-  
+  <salary />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import salary from "@/components/salary.vue";
+import salary from "@/components/salary.vue";
 
 export default {
   name: "Home",
   components: {
-    
+    salary
   }
 };
 </script>
