182dd0a8099b3d40f89d15c1b79ba0162a1d6384
Taive Sarnik <taive.sarnik@gmail.com>
2021-03-15 18:24:37 +0200

    edited Salary.vue view

--- src/views/Salary.vue
+++ src/views/Salary.vue
@@ -1,50 +1,17 @@
 <template>
-  <v-container>
-    <v-col class="text-center">
-      <v-text-field
-        v-model.number="a"
-        label="Sisesta siia number A"
-        hide-details="auto"
-        type="number"
-      ></v-text-field>
-      <v-text-field
-        v-model.number="b"
-        label="Sisesta siia number B"
-        hide-details="auto"
-        type="number"
-      ></v-text-field>
-      <h1>Nende arvude summa on: {{ result }}</h1>
-      <line-chart :chartdata="chartData" :options="chartOptions"/>
-    </v-col>
-  </v-container>
+  <div>
+  <salary />
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import salary from "@/components/salary.vue";
+
 export default {
   name: "Salary",
-  data() {
-    return {
-      a: 0,
-      b: 0,
-    };
-  },
-  computed: {
-    result() {
-      return this.addAB(this.a, this.b);
-    },
-  },
-  //watch: {
-  //a(val) {
-  //this.addAB(val, this.b)
-  //},
-  //b(val) {
-  //this.addAB(val, this.a)
-  //}
-  //},
-  methods: {
-    addAB(a, b) {
-      return +a + +b;
-    },
-  },
+  components: {
+    salary
+  }
 };
 </script>
