fe5e9c2e0b349b6da2136a28e3212d68aedee249
Taive Sarnik <taive.sarnik@gmail.com>
2021-03-16 17:23:09 +0200

    added results table

--- src/components/salary.vue
+++ src/components/salary.vue
@@ -1,25 +1,126 @@
 <template>
   <v-container>
-    <v-col md="4">juhuu</v-col>
-    <v-col class="text-center" justify="center" 
-      md="4">
-      <v-text-field
-        v-model.number="a"
-        label="Sisesta siia number A"
-        hide-details="auto"
-        type="number"
-        class="pa-4 secondary text-center rounded-pill"
-      ></v-text-field>
-      <v-text-field
-        v-model.number="b"
-        label="Sisesta siia number B"
-        hide-details="auto"
-        type="number"
-        class="pa-4 secondary text-center rounded-pill"
-      ></v-text-field>
-      <h1>Nende arvude summa on: {{ result }}</h1>
-    </v-col>
-    <v-col md="4">mis sa teed?</v-col>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="6">
+        <h3 align="center">Sisesta siia oma brutopalk:</h3>
+        <v-text-field
+          v-model.number="salary"
+          hide-details="auto"
+          type="number"
+          class="pa-4 secondary text-center rounded-pill"
+        ></v-text-field>
+        <v-text-field
+          v-model.number="b"
+          label="Sisesta siia number B"
+          hide-details="auto"
+          type="number"
+          class="pa-4 secondary text-center rounded-pill"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Arvesta sotsiaalmaksu"
+          type="checkbox"
+          color="secondary"
+          required
+        ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Arvesta maksuvaba tulu"
+          type="checkbox"
+          color="secondary"
+          required
+        ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Tööandja töötuskindlustusmakse 0,8%"
+          type="checkbox"
+          color="secondary"
+          required
+        ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Töötaja töötuskindlustusmakse 1,6%"
+          type="checkbox"
+          color="secondary"
+          required
+        ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Kogumispension (II sammas)"
+          type="checkbox"
+          color="secondary"
+          required
+        ></v-checkbox>
+      </v-col>
+    </v-row>
+    <v-simple-table class="mb-7 mx-auto">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="pa-3 secondary text-center rounded-pill">
+              TÖÖANDJA:
+            </th>
+            <th class="text-center">Summa</th>
+            <th class="text-center">
+              Protsent
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="text-center"
+            v-for="item in employerTableItems"
+            :key="item.itemName"
+          >
+            <td class="pa-3 primary text-center rounded-pill">
+              {{ item.itemName }}
+            </td>
+            <td>{{ item.resultAsMoney }}</td>
+            <td>{{ item.resultAsPercent }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+<v-simple-table class="mb-7 mx-auto">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="pa-3 secondary text-center rounded-pill">
+              TÖÖTAJA:
+            </th>
+            <th class="text-center">Summa</th>
+            <th class="text-center">
+              Protsent
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="text-center"
+            v-for="item in employeeTableItems"
+            :key="item.itemName"
+          >
+            <td class="pa-3 primary text-center rounded-pill">
+              {{ item.itemName }}
+            </td>
+            <td>{{ item.resultAsMoney }}</td>
+            <td>{{ item.resultAsPercent }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-container>
 </template>
 
@@ -31,18 +132,68 @@ export default {
   },
   data() {
     return {
-      a: 0,
+      salary: 0,
       b: 0,
+      employerTableItems: [
+        {
+          itemName: "Palgafond kokku",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Brutopalk",
+          resultAsMoney:  this.salary,
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Sotsiaalmaks",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Töötuskindlustus",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+      ],
+
+      employeeTableItems: [
+        {
+          itemName: "Brutopalk",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Töötuskindlustus",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Kogumispension",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Tulumaks",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI",
+        },
+        {
+          itemName: "Netopalk",
+          resultAsMoney: "MIDAGI!",
+          resultAsPercent: "MIDAGI!",
+        },
+      ],
     };
   },
   computed: {
     result() {
-      return this.addAB(this.a, this.b);
+      return this.addAB(this.salary, this.b);
     },
   },
   methods: {
-    addAB(a, b) {
-      return +a + +b;
+    addAB(salary, b) {
+      return +salary + +b;
     },
   },
 };
