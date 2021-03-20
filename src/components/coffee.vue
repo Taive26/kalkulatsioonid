8baa391f113bec83dffe1f02cdf7894c8f1d386c
Ititriin <triin.tajur@vikk.ee>
2021-03-20 21:42:26 +0200

    tiny addition to home page

--- src/components/coffee.vue
+++ src/components/coffee.vue
@@ -1,14 +1,13 @@
 <template>
   <v-container>
-<!--
+    <!--
 PLAAN
 1) arvutada lethalDose kasutaja sisestatud kehamassi jaoks (200mg/kg kohta) - saab kasutaja surmava doosi
 2) arvutada caffeineConsumed vastavalt valitud kangusele 
 3) lethalDose - caffeineConsumed = caffeineTillDeath
 4) konvertida caffeineTillDeath tassideks ja KUVADA "Võid juua veel {{cupsTillDeath}} tassi "
 -->
-    <v-row align="center"
-      justify="center">
+    <v-row align="center" justify="center">
       <v-col sm="4" md="4">
         <v-row>
           <div id="app" align="center">
@@ -24,19 +23,29 @@ PLAAN
           ><div>
             <v-text-field
               type="number"
-              label="Tarbitud tasside arv" outlined
+              label="Tarbitud tasside arv"
+              outlined
             ></v-text-field></div
         ></v-row>
         <v-row
-          ><div><v-select :items="intensity" label="Kangus" outlined></v-select></div
+          ><div>
+            <v-select
+              :items="intensity"
+              label="Kangus"
+              outlined
+            ></v-select></div
         ></v-row>
         <v-row
           ><div>
             <v-text-field
               type="number"
-              label="Kehamass (kg)" outlined
+              label="Kehamass (kg)"
+              outlined
             ></v-text-field></div
         ></v-row>
+        <v-row class="mt-10">
+          <h2>Võid juua veel ... tassi</h2>
+        </v-row>
       </v-col>
       <v-col sm="1" md="1">
         <v-row
@@ -45,10 +54,7 @@ PLAAN
             alt="coffee cups"
             class="d-flex flex-column justify-space-between align-center"
           >
-            <img
-              class="rightpane"
-              src="@/assets/kohvitassid.png"
-            /></div
+            <img class="rightpane" src="@/assets/kohvitassid.png" /></div
         ></v-row>
       </v-col>
     </v-row>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,16 +1,22 @@
 <template>
   <v-container>
-    <v-row>
-      <h2>Tšau!</h2>
-    </v-row>
+    <v-row align="center" justify="center">
+      <div id="app" align="center">
+        <img class="home" src="@/assets/avaleht.png" alt="" /></div
+    ></v-row>
   </v-container>
 </template>
+<style scoped>
+img.home {
+  width: 1000px;
+}
+</style>
 <script>
 export default {
   name: "Home",
   data() {
     return {};
   },
-  methods: {}
+  methods: {},
 };
 </script>
