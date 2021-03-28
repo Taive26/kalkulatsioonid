d97f77b00aceeeb91b97df3089e14b0c641075fa
Ititriin <triin.tajur@vikk.ee>
2021-03-28 21:11:16 +0300

    mobile look additions

--- src/components/fuel.vue
+++ src/components/fuel.vue
@@ -1,14 +1,43 @@
 <template>
-  <v-container>
-    <v-row class="mt-40">
-      <input type="radio" name="options" @change="onChange($event)" value="journeyLength" />
-      Teepikkus
-      <input type="radio" name="options" @change="onChange($event)" value="fuelConsumption" />
-      Kütuse kulu
-      <input type="radio" name="options" @change="onChange($event)" value="fuelAmount" /> 
-      Kütuse kogus
+  <v-container class="secondary">
+    <v-row class="mt-40 centered-input rounded-pill center">
+      <img
+        class="leftpane-mob leftpane"
+        src="@/assets/fuelgauge.png"
+        alt="By Videoplasty.com, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=67046674"
+      />
 
-      <v-col sm="12" md="4">
+      <v-col sm="12" md="4" class="radiobuttons">
+        <v-row class="pa-2 primary rounded-pill row-space">
+          <input
+            type="radio"
+            name="options"
+            @change="onChange($event)"
+            value="journeyLength"
+          />Teepikkus
+        </v-row>
+        <v-row class="pa-2 primary rounded-pill row-space">
+          <input
+            type="radio"
+            name="options"
+            @change="onChange($event)"
+            value="fuelConsumption"
+          />Kütuse kulu
+        </v-row>
+        <v-row class="pa-2 primary rounded-pill row-space">
+          <input
+            type="radio"
+            name="options"
+            @change="onChange($event)"
+            value="fuelAmount"
+          />Kütuse kogus
+        </v-row>
+        <v-row class="pa-2 primary rounded-pill row-space">
+          <h4 align="center" justify="center">Kütuse hind: {{ result }} EUR</h4>
+        </v-row>
+      </v-col>
+
+      <v-col sm="12" md="4" class="textboxes">
         <v-text-field
           label="Teekonna pikkus"
           ref="journeyLength"
@@ -16,7 +45,7 @@
           id="journeyLength"
           name="journeyLength"
           type="number"
-          class="form-control rounded-pill"
+          class="form-control primary rounded-pill center"
           v-on:keyup="check"
           filled
         >
@@ -28,7 +57,7 @@
           id="fuelConsumption"
           name="fuelConsumption"
           type="number"
-          class="form-control rounded-pill"
+          class="form-control primary rounded-pill"
           v-on:keyup="check"
           filled
         >
@@ -40,7 +69,7 @@
           id="fuelAmount"
           name="fuelAmount"
           type="number"
-          class="form-control rounded-pill"
+          class="form-control primary rounded-pill"
           v-on:keyup="check"
           filled
         >
@@ -52,14 +81,9 @@
           filled
           id="fuelCost"
           name="fuelCost"
-          class="rounded-pill"
+          class="primary rounded-pill"
         >
         </v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="mt-10">
-      <v-col sm="12" md="4">
-        <h2>Kütuse maksumus: {{ result }} EUR</h2>
       </v-col>
     </v-row>
   </v-container>
@@ -68,6 +92,54 @@
 <style scoped>
 .mt-40 {
   margin-top: 40px;
+}
+
+input[type="radio"] {
+  margin-right: 10px;
+}
+
+.row-space {
+  margin-top: 30px;
+}
+
+.radiobuttons {
+  margin: auto;
+  width: 30%;
+  padding: 10px;
+  font-size: 1.25rem;
+}
+
+.textboxes {
+  margin: auto;
+  width: 70%;
+}
+
+img.leftpane {
+  width: 30%;
+  height: 30%;
+}
+
+@media screen and (max-width: 759px) {
+  .hidden {
+    display: none;
+  }
+
+  .mt-40 {
+    margin-top: 0px;
+  }
+
+  img.leftpane-mob {
+    width: 56%;
+    height: auto;
+  }
+
+  input[type="radio"] {
+    margin-right: 0px;
+  }
+
+  .row-space {
+    margin-top: 0px;
+  }
 }
 </style>
 
@@ -85,25 +157,29 @@ export default {
       radioBoxOption: String,
       journeyUnits: [
         { name: "Kilomeeter (km)", id: 1, value: 1 },
-        { name: "Miil (mi)", id: 2, value: 1.609344 }
+        { name: "Miil (mi)", id: 2, value: 1.609344 },
       ],
       consumptionUnits: [
         { name: "Liitrit / 100 km", id: 1, value: 1 },
-        { name: "Liitrit / 100 mi", id: 2, value: 1.609344 }
+        { name: "Liitrit / 100 mi", id: 2, value: 1.609344 },
       ],
       fuelUnits: [
         { name: "Liiter (L)", id: 1, value: 1 },
-        { name: "US gallon (liquid gal)", id: 2, value: 1.609344 }
+        { name: "US gallon (liquid gal)", id: 2, value: 1.609344 },
       ],
       journeyLength: 1200,
       fuelConsumption: 5.7,
       fuelAmount: 68.4,
-      fuelCost: 1.3
+      fuelCost: 1.3,
     };
   },
   computed: {
     result() {
-      return this.sum(this.journeyLength, this.fuelConsumption, this.fuelCost).toFixed(2);
+      return this.sum(
+        this.journeyLength,
+        this.fuelConsumption,
+        this.fuelCost
+      ).toFixed(2);
     },
   },
   methods: {
@@ -118,7 +194,7 @@ export default {
     check: function() {
       console.log(this.journeyLength);
       console.log(this.fuelConsumption);
-      console.log(this.fuelAmount); 
+      console.log(this.fuelAmount);
       console.log(this.options);
 
       if (this.options === "journeyLength") {
@@ -155,7 +231,7 @@ export default {
 
     onChange(event) {
       this.options = event.target.value;
-    }
-  }
+    },
+  },
 };
 </script>
