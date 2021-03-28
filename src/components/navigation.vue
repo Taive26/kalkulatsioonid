d2e1ebec337102c99598266a5363470dd1200dc4
Ititriin <triin.tajur@vikk.ee>
2021-03-28 17:47:11 +0300

    correcting typo

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,20 +1,12 @@
 <template>
-  <nav :style="{ background: background || '#f0f184' }">
-    <ul :style="{ background: background || '#f0f184' }" ref="nav">
-      <figure class="image-logo" @click="toggleNav">
-        <img :src="imagePath" height="40px" width="40px" />
+  <nav>
+    <ul ref="nav">
+      <figure @click="toggleNav">
+        <img/>
       </figure>
-      <li 
-        v-for="(link, index) in navLinks"
-        :key="index"
-        @mouseenter="
-          $event.currentTarget.style.background = hoverBackground || '#999'
-        "
-        @mouseleave="
-          $event.currentTarget.style.background = background || '#f0f184'
-        "
-      >
-        <router-link :to="link.path" :style="{ color: linkColor || '#DDD' }">
+      <li v-for="(link, index) in navLinks"
+        :key="index">
+        <router-link :to="link.path">
           {{ link.text }}
           <i class="mob" :class="link.icon" />
         </router-link>
@@ -26,11 +18,7 @@
 <script>
 export default {
   props: [
-    "navLinks",
-    "background",
-    "linkColor",
-    "hoverBackground",
-    "imagePath",
+    "navLinks"
   ],
   methods: {
     toggleNav() {
@@ -41,73 +29,3 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-nav {
-  height: 60px;
-  width: 100%;
-  box-shadow: 2px 2px 2px #ffffff;
-  ul {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    margin-block-start: 0;
-    margin-block-end: 0;
-    padding-inline-start: 0;
-    box-shadow: 2px 2px 2px #ffffff;
-    figure {
-      cursor: pointer;
-      margin-right: 10px;
-    }
-    a {
-      text-decoration: none;
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-    }
-    i {
-      margin-right: 10px;
-      font-size: 22px;
-    }
-    li {
-      list-style-type: none;
-      padding: 10px 20px;
-    }
-  }
-}
-@media screen and (max-width: 759px) {
-  nav {
-    ul {
-      position: absolute;
-      width: 200px;
-      flex-direction: column;
-      left: -240px;
-      transition: 300ms ease all;
-      top: 60px;
-      &.active {
-        left: 0px;
-      }
-      figure {
-        position: fixed;
-        z-index: 1;
-        top: 10px;
-        left: 2px;
-      }
-      li {
-        width: 100%;
-        padding-left: 0;
-        padding-right: 0;
-      }
-      a {
-        flex-direction: row;
-        margin-left: 10px;
-        justify-content: space-between;
-        margin-right: 13px;
-        background-color: #f0f184;
-      }
-    }
-  }
-  .mob {
-    display: none;
-  }
-}
-</style>
