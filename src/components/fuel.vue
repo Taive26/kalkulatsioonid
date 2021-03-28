9448303dec62406dd9f0b9ed669468f1d72c5513
Ititriin <triin.tajur@vikk.ee>
2021-03-28 12:09:53 +0300

    fuel calculator change of logic

--- src/components/fuel.vue
+++ src/components/fuel.vue
@@ -1,33 +1,83 @@
 <template>
   <v-container>
-    <v-row class="mt-40" justify="center">
-      <v-col sm="12" md="4">
-        <v-checkbox
-          label="Teekonna pikkus" color="warning" v-on:change="Onchange()" v-model="checkboxval1"></v-checkbox>
-        <v-checkbox
-          label="Kütusekulu" color="warning" v-on:change="Onchange()" v-model="checkboxval2"></v-checkbox>
-        <v-checkbox
-          label="Kütuse kogus" color="warning" v-on:change="Onchange()" v-model="checkboxval3"></v-checkbox>
+    <v-row class="mt-40">
+      <v-col sm="12" md="4" align="center" justify="center">
+        <v-radio-group v-model="column" column>
+          <v-radio
+            label="Teekonna pikkus"
+            value="radio-1"
+            @click="isDisabled1 = !isDisabled1"
+          ></v-radio>
+          <v-radio
+            label="Kütuse kulu"
+            value="radio-2"
+            @click="isDisabled2 = !isDisabled2"
+          ></v-radio>
+          <v-radio
+            label="Kütuse kogus"
+            value="radio-3"
+            @click="isDisabled3 = !isDisabled3"
+          ></v-radio>
+        </v-radio-group>
       </v-col>
       <v-col sm="12" md="4">
         <v-text-field
-          label="Teekonna pikkus" v-model="journeyLength" v-on:click="Onchange()" type="number" class="form-control" 
-          v-bind:disabled="checkbox1" outlined></v-text-field>
+          label="Teekonna pikkus"
+          :disabled="isDisabled1"
+          v-model.trim="journeyLength"
+          id="journeyLength"
+          name="journeyLength"
+          type="number"
+          class="form-control"
+          v-on:click="check"
+          filled
+        >
+        </v-text-field>
         <v-text-field
-          label="Keskmine kütusekulu" v-model="fuelConsumption" type="number" class="form-control"
-          v-bind:disabled="checkbox2" outlined></v-text-field>
+          label="Keskmine kütusekulu"
+          :disabled="isDisabled2"
+          v-model.trim="fuelConsumption"
+          id="fuelConsumption"
+          name="fuelConsumption"
+          type="number"
+          class="form-control"
+          v-on:click="check"
+          filled
+        >
+        </v-text-field>
         <v-text-field
-          label="Kütuse kogus" v-model="fuelAmount" type="number" class="form-control" 
-          v-bind:disabled="checkbox3" outlined></v-text-field>
+          label="Kütuse kogus"
+          :disabled="isDisabled3"
+          v-model.trim="fuelAmount"
+          id="fuelAmount"
+          name="fuelAmount"
+          type="number"
+          class="form-control"
+          v-on:click="check"
+          filled
+        >
+        </v-text-field>
         <v-text-field
-          label="Kütuseühiku hind" v-model="fuelCost" type="number" outlined></v-text-field>
+          label="Kütuseühiku hind"
+          v-model.trim="fuelCost"
+          type="number"
+          filled
+          id="fuelCost"
+          name="fuelCost"
+        >
+        </v-text-field>
       </v-col>
       <v-col md="4" class="hidden">
-        <v-select
-          :items="journeyUnits" label="Kilomeeter(km)" outlined></v-select>
-        <v-select
-          :items="consumptionUnits" label="Liitrit/100km" outlined></v-select>
+        <v-select class="form-control">
+        <option v-for="name in journeyUnits" :value="journeyUnits.value" :key="name"></option>
+        </v-select>
+        <v-select :items="consumptionUnits" label="Liitrit/100km" outlined>
+        </v-select>
         <v-select :items="fuelUnits" label="Liiter(l)" outlined></v-select>
+        <select class="form-control">
+          <option
+            v-for="country in countries" :value="country.code" :key="country.code" >{{ country.name }}</option>
+        </select>
       </v-col>
     </v-row>
     <v-row class="mt-10">
@@ -51,19 +101,37 @@ export default {
     msg: String,
   },
 
+  /* 1. TeePikkus = KütuseKogus/KütuseKulu * 100
+2. KütuseKulu = KütuseKogus/TeePikkus * 100
+3. KütuseKogus = TeePikkus/100 * KütuseKulu */
+
+  fuelConsumption(fuelAmount, journeyLength) {
+    return (fuelAmount / journeyLength) * 100;
+  },
+
+  fuelAmount(journeyLength, fuelConsumption) {
+    return (journeyLength / 100) * fuelConsumption;
+  },
+
   data() {
     return {
-      checkbox1: false,
-      checkbox2: false,
-      checkbox3: false,
-      checkboxval1: false,
-      checkboxval2: false,
-      checkboxval3: false,
-      journeyLength: 0,
-      fuelConsumption: 0,
-      fuelAmount: 0,
-      fuelCost: 0,
-      journeyUnits: ["Kilomeeter(km)"],
+      isDisabled1: false,
+      isDisabled2: false,
+      isDisabled3: false,
+      countries: [
+        { code: "GB", name: "Great Britain" },
+        { code: "US", name: "United States" },
+        { code: "KZ", name: "Kazakhstan" },
+      ],
+      selectedCountry: null,
+      journeyUnits: [
+        { name: "Kilomeeter (km)", id: 1, value: 1 },
+        { name: "Miil (mi)", id: 2, value: 1.609344 },
+      ],
+      journeyLength: 1200,
+      fuelConsumption: 5.7,
+      fuelAmount: 68.4,
+      fuelCost: 1.3,
       consumptionUnits: ["Liitrit/100km"],
       fuelUnits: ["Liiter(l)"],
     };
@@ -74,11 +142,6 @@ export default {
     },
   },
   methods: {
-    Onchange: function() {
-      this.checkbox1 = this.checkboxval1;
-      this.checkbox2 = this.checkboxval2;
-      this.checkbox3 = this.checkboxval3;
-    },
     sum(journeyLength, fuelConsumption, fuelCost) {
       const input = (+journeyLength / +100) * +fuelConsumption * +fuelCost;
       if (Number.isFinite(input)) {
@@ -87,29 +150,17 @@ export default {
         return 0;
       }
     },
-    journeyLengthValue(fuelAmount, fuelConsumption) {
-      const input = +fuelAmount / (+fuelConsumption / +100);
-      if (Number.isFinite(input)) {
-        return +fuelAmount / (+fuelConsumption / +100);
-      } else {
-        return 0;
-      }
+
+    check: function(event) {
+      alert(event.target.tagName);
+      return this.journeyLengthCalculation(
+        this.fuelAmount,
+        this.fuelConsumption
+      );
     },
-    fuelConsumptionValue(fuelAmount, journeyLength) {
-      const input = +fuelAmount / (+journeyLength / +100);
-      if (Number.isFinite(input)) {
-        return +fuelAmount / (+journeyLength / +100);
-      } else {
-        return 0;
-      }
-    },
-    fuelAmountValue(fuelConsumption, journeyLength) {
-      const input = +fuelConsumption * (+journeyLength / +100);
-      if (Number.isFinite(input)) {
-        return +fuelConsumption * (+journeyLength / +100);
-      } else {
-        return 0;
-      }
+
+    journeyLengthCalculation(fuelAmount, fuelConsumption) {
+      return (fuelAmount / fuelConsumption) * 100;
     },
   },
 };
